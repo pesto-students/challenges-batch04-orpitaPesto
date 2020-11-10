d7e9d0093c49f90310ec41b427aeede05457bf56
Orpita Dutta <62539093+orpitaPesto@users.noreply.github.com>
2020-11-10 16:54:05 +0530

    Updated allSettled.js

--- js-exercises/all-settled/allSettled.js
+++ js-exercises/all-settled/allSettled.js
@@ -1,2 +1,17 @@
-
+const allSettled = (promises) => new Promise((resolve) => {
+  let indexOfPromise = promises.length - 1;
+  const results = [];
+  const resolver = (result, index) => {
+    indexOfPromise -= 1;
+    results[index] = { status: "fulfilled", value: result };
+    if (indexOfPromise === 0) {
+      resolve(results);
+    }
+  };
+  promises.forEach((promise, index) => {
+    Promise.resolve(promise)
+      .then((result) => resolver(result, index))
+      .catch((error) => results.push({status: "rejected", reason: error}));
+  });
+});
 export { allSettled };
