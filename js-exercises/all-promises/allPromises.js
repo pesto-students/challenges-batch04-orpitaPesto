1763d2651ea2668aa2cb3311c02022f09041786e
Orpita Dutta <62539093+orpitaPesto@users.noreply.github.com>
2020-11-10 16:44:02 +0530

    Updated allPromise.js

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,3 +1,18 @@
-const allPromises = args => {};
+const allPromises = (promises) => new Promise((resolve) => {
+  let indexOfPromise = promises.length - 1;
+  const results = [];
+  const resolver = (result, index) => {
+    indexOfPromise -= 1;
+    results[index] = result;
+    if (indexOfPromise === 0) {
+      resolve(results);
+    }
+  };
+  promises.forEach((promise, index) => {
+    Promise.resolve(promise)
+      .then((result) => resolver(result, index))
+      .catch((error) => reject(error));
+  });
+});
 
 export { allPromises };
