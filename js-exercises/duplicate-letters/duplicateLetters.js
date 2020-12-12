cbf7c1fcaf0b6185f8d915d217e88b1b7d0f5ab4
Orpita Dutta <62539093+orpitaPesto@users.noreply.github.com>
2020-12-12 12:52:02 +0530

    Update duplicateLetters.js
    V2.0

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,19 +1,29 @@
-function duplicateLetters(text) {
-  const textArray = text.split('');
-  const duplicateList = {};
-  let largestDuplicateNumer = 1;
-  textArray.forEach((letter) => {
-    if (duplicateList[letter]) {
-      duplicateList[letter] += 1;
-      if (duplicateList[letter] > largestDuplicateNumer) {
-        largestDuplicateNumer = duplicateList[letter];
-      }
-    } else {
-      duplicateList[letter] = 1;
-    }
-  });
-  return largestDuplicateNumer === 1 ? false : largestDuplicateNumer;
+function duplicateLetters(string){
+    let arrayOfCharacters = [...string];
+    let objectString = {};
+    arrayOfCharacters.map((character)=> {
+        objectString[character]? objectString[character] +=1 :objectString[character]=1
+    });
+    let maxRepititions = Math.max.apply(Math, Object.values(objectString));
+    return maxRepititions === 1?false:maxRepititions;
 }
+
+// function duplicateLetters(text) {
+//   const textArray = text.split('');
+//   const duplicateList = {};
+//   let largestDuplicateNumer = 1;
+//   textArray.forEach((letter) => {
+//     if (duplicateList[letter]) {
+//       duplicateList[letter] += 1;
+//       if (duplicateList[letter] > largestDuplicateNumer) {
+//         largestDuplicateNumer = duplicateList[letter];
+//       }
+//     } else {
+//       duplicateList[letter] = 1;
+//     }
+//   });
+//   return largestDuplicateNumer === 1 ? false : largestDuplicateNumer;
+// }
 
 export {
   duplicateLetters,
